e4c3889bff6b91156b6a7052c009cfebb70c3480
EmmanuelAyanleye <ayanleyeemmanuel5@gmail.com>
2025-09-24 18:42:16 +0100

    Production

--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6zumh&r+d5@$c04imz*@1sm^dhe6mhz&(r5@ee#h-+8b#ovgv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
